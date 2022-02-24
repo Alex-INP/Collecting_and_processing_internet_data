a526b8c64ec364725fd94309d3a84a9e2e331b60
Alex <a.a.alexashkin@mail.ru>
2022-02-24 10:56:24 +0300

    Lesson_1_Tasks_0

--- Task_1.py
+++ Task_1.py
@@ -1,7 +1,11 @@
 import requests
+import json
 
 name = "Alex-INP"
 result = requests.get(f"https://api.github.com/users/{name}/repos").content
+
+for i in json.loads(result):
+	print(i["name"])
 
 with open("results.json", "wb") as file:
 	file.write(result)
