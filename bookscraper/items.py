5c658723a84a7a1c7308b33e3c7e0768ed6de287
Alex <a.a.alexashkin@mail.ru>
2022-03-24 12:08:52 +0300

    Lesson_7_Tasks

--- bookscraper/items.py
+++ bookscraper/items.py
@@ -4,7 +4,14 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from itemloaders.processors import MapCompose, TakeFirst
 
+def process_price(num):
+    try:
+        num = int(num)
+    except:
+        pass
+    return num
 
 class BookscraperItem(scrapy.Item):
     # define the fields for your item here like:
@@ -15,3 +22,10 @@ class BookscraperItem(scrapy.Item):
     price = scrapy.Field()
     discount_price = scrapy.Field()
     rating = scrapy.Field()
+
+class LeroyscraperItem(scrapy.Item):
+    name = scrapy.Field(output_processor=TakeFirst())
+    pictures = scrapy.Field()
+    link = scrapy.Field(output_processor=TakeFirst())
+    price = scrapy.Field(input_processor=MapCompose(process_price), output_processor=TakeFirst())
+
--- bookscraper/pipelines.py
+++ bookscraper/pipelines.py
@@ -5,8 +5,11 @@
 
 
 # useful for handling different item types with a single interface
+import scrapy
 from itemadapter import ItemAdapter
+import re
 from pymongo import MongoClient
+from scrapy.pipelines.images import ImagesPipeline
 
 
 class BookscraperPipeline:
@@ -71,3 +74,34 @@ class BookscraperPipeline:
             if item["rating"][-1] == " ":
                 item["rating"] = item["rating"][:-1]
 
+
+class LeroyParserPipeline:
+    def process_item(self, item, spider):
+        return item
+
+
+class LeroyPhotosPipeline(ImagesPipeline):
+    def get_media_requests(self, item, info):
+        if item["pictures"]:
+            for ind, p_link in enumerate(item["pictures"]):
+                width = self.get_position_data("w_82", p_link)
+                p_link = p_link.replace(p_link[width[0]:width[1]], "w_2000")
+
+                height = self.get_position_data("h_82", p_link)
+                p_link = p_link.replace(p_link[height[0]:height[1]], "h_2000")
+
+                item["pictures"][ind] = p_link
+
+                for link in item["pictures"]:
+                    try:
+                        yield scrapy.Request(link)
+                    except Exception as e:
+                        print(e)
+
+    def item_completed(self, results, item, info):
+        item["pictures"] = [i[1] for i in results if i[0]]
+        return item
+
+    def get_position_data(self, segment, full_text):
+        result = re.search(fr"{segment}", full_text)
+        return [result.start(), result.end()]
